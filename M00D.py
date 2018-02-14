186e5f0ba52cc26a5d0dc6a7d9f018a127c92644
Dylan Hamer <dylanhamer13@gmail.com>
2018-02-14 21:21:32 +0000

    Fixed header

---
+++ M00D.py
@@ -0,0 +1,35 @@
+"""
+Alone.py
+"""
+
+import random, click, time, sys
+
+"""Variables that modify the behaviour of the display"""
+class variables:
+    colours = ["blue", "cyan", "white"]
+    text = ["drip ", "drop ", "splash "]
+    delay = 1
+    title = "It\'s raining..."
+
+"""Aesthetic generator"""
+def mood(**kwargs):
+    colours = kwargs.get("colours", ["blue"])
+    text = kwargs.get("text", ["m00d"])
+    delay = kwargs.get("delay", 1)
+    title = kwargs.get("title", "M00D")
+
+    click.clear()
+
+    sys.stdout.write("\x1b]2;{}\x07".format(title))
+
+    while True:
+        currentColour = random.choice(colours)
+        currentText = random.choice(text)
+
+        click.secho(currentText, fg=currentColour, nl=False)
+        time.sleep(delay)
+
+mood(colours = variables.colours,
+     text = variables.text,
+     delay = variables.delay,
+     title = variables.title)
---
+++ README.md
@@ -0,0 +1,7 @@
+# M00D
+
+Neat little aesthetic generator. Very calming. 
+Modify the script to change the mood.
+
+by Dylan Hamer
+
