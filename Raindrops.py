18749d058518d04645cd460df19023f847122e22
Dylan Hamer <dylanhamer13@gmail.com>
2018-02-15 14:48:35 +0000

    Added some new features to Raindrops.py

--- Raindrops.py
+++ Raindrops.py
@@ -9,8 +9,12 @@ from M00D import mood
 class raindrops:
     colours = ["blue", "cyan", "white"]
     text = ["drip ", "drop ", "splash "]
+    title = "It\'s raining..."
+    exitMessage = "\n\nIt has stopped raining. The sun is shining. In the distance, a rainbow is forming.\n"
     delay = 1
 
 mood(colours = raindrops.colours,
      text = raindrops.text,
-     delay = raindrops.delay)
+     delay = raindrops.delay
+     title = raindrops.title,
+     exitMessage = raindrops.exitMessage)
