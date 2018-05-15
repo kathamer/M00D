47df381b8d772caf2ea0367ef287cba274229e82
Dylan Hamer <dylanhamer13@gmail.com>
2018-05-15 13:17:20 +0100

    Cleaned up code

--- Examples/Raindrops.py
+++ Examples/Raindrops.py
@@ -4,19 +4,15 @@ Raindrop aesthetic for M00D
 by Dylan Hamer
 """
 
-from M00D import loop, clear
+from . import mood
 
-raindrops
-    colours = ["blue", "cyan", "white"]
-    text = ["drip ", "drop ", "splash "]
-    title = "It\'s raining..."
-    exitMessage = "\n\nIt has stopped raining. The sun is shining. In the distance, a rainbow is forming.\n"
-    delay = 0.1
+"""Dictionary defining generator"""
+raindrops = {"colours":["blue", "cyan", "white"],
+            "text":["drip ", "drop ", "splash "],
+            "title":"It\'s raining...",
+            "exitMessage":"\n\nIt has stopped raining. The sun is shining. In the distance, a rainbow is forming.\n",
+            "delay":0.1}
 
-clear()
+mood.clear()
 
-loop(colours = raindrops.colours,
-     text = raindrops.text,
-     delay = raindrops.delay,
-     title = raindrops.title,
-     exitMessage = raindrops.exitMessage)
+mood.loop(**raindrops)  # Run loop with argument dictionary
